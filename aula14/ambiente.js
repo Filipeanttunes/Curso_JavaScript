635f7b0b7ca073f0639585d39be866bf6c72b705
Filipe <filipeanttunes@gmail.com>
2021-07-21 20:21:54 -0300

    Exercício 6

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,5 +1,30 @@
 console.log('Vai começar')
-for(var c = 1;c <= 5;c++){
-console.log(c)
+// for(var c = 1;c <= 5;c++){
+// console.log(c)
+// }
+// console.log('Fim!')
+
+const carros = ['gol', 'palio', 'celta', 'UNO(A Lenda)'];
+
+function pintura(modelo){
+    let cor;
+    if(modelo.length <= 3){
+        cor = 'vermelho'
+    } else if(modelo.length <= 5 && modelo.length > 3){
+        cor = 'preto'
+    } else{
+        cor = 'azul'
+    }
+    return cor
 }
-console.log('Fim!')
+
+
+const carrosAfter = carros.map(carro => {
+    const result = {
+        Modelo: carro,
+        cor: pintura(carro)
+    }
+    return result;
+})
+
+console.log(carrosAfter);
