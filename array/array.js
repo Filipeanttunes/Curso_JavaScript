901f541283e352d6402380aced5f9e2b34062932
Filipe <filipeanttunes@gmail.com>
2021-08-05 20:47:17 -0300

    Array

---
+++ array/array.js
@@ -0,0 +1,30 @@
+console.log(typeof Array, typeof new Array, typeof [])
+
+let aprovados = new Array('Bia', 'Carlos', 'Ana')
+console.log(aprovados)
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+console.log(aprovados[0])
+console.log(aprovados[1])
+console.log(aprovados[2])
+console.log(aprovados[3])
+
+aprovados[3]= 'Paulo' //adiciona
+aprovados.push('Abia') //adiciona
+console.log(aprovados.length)
+
+aprovados[9] = 'Rafael'
+console.log(aprovados.length)
+console.log(aprovados[8] === undefined)
+
+console.log(aprovados)
+aprovados.sort() //Ordena
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(aprovados[1])
+console.log(aprovados[2])
+
+aprovados = ['Bia', 'Carlos', 'Ana']
+aprovados.splice(1, 1, 'elemento1') // pode adicionar e remover elementos
+console.log(aprovados)
